c1a75a0a817242afa86a4a158a6b80656356c24d
Amal Arul <amalarul200620@gmail.com>
2026-06-10 14:52:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openImg(src){
+document.getElementById('popup').style.display='flex';
+document.getElementById('popupimg').src=src;
+}
+function closeImg(){
+document.getElementById('popup').style.display='none';
+}
